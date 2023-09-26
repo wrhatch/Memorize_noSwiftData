408c0f8d3fb1ea889161f99826eb39a2a97a2cae
Westin Hatch <wrhatch@icloud.com>
2023-09-26 15:43:59 -0600

    Initial Commit

---
+++ Memorize_noSwiftData/Memorize_noSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_noSwiftDataApp.swift
+//  Memorize_noSwiftData
+//
+//  Created by Westin Hatch on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_noSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
